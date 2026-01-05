57d97954792b63290d0dad71f42b1c27542582e9
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-01-05 23:47:06 +0530

    extra code

--- ch-3-expressjs/index.js
+++ ch-3-expressjs/index.js
@@ -9,10 +9,10 @@ const express = require('express');
 const server = express();
 
 
-// body parser middleware
+// body parser middleware or built-in middleware , it is used to parse the incoming request body in json format
 server.use(express.json()); 
 
-// third party middleware
+// third party or external middleware , it is used to log the request details in console
 const morgan = require('morgan');
 server.use(morgan('dev'));
 
@@ -20,23 +20,23 @@ server.use(morgan('dev'));
 server.use(express.static('public'));
 
 
-// //custom middleware or application level middleware
-// server.use((req,res,next)=>{
-//   console.log(req.method , req.ip , req.url , new Date() , req.get('User-Agent'));
-//   next();
-// }) 
+//custom middleware or application level middleware
+server.use((req,res,next)=>{
+  console.log(req.method , req.ip , req.url , new Date() , req.get('User-Agent'));
+  next();
+}) 
 
-// // router level middleware
-// // it is used just to chech in postman in params password=12345 
-// const auth1 = (req ,res, next)=>{
-//   // console.log(req.query)
-//   if(req.query.password === '12345'){
-//     console.log('Authenticated');
-//     next();
-//   }else{
-//     res.status(401).send('Unauthorized');
-//   }
-// }
+
+// it is used just to chech in postman in params password=12345 
+const auth1 = (req ,res, next)=>{
+  // console.log(req.query)
+  if(req.query.password === '12345'){
+    console.log('Authenticated');
+    next();
+  }else{
+    res.status(401).send('Unauthorized');
+  }
+}
 
 // it is used just to chech in postman in body password=12345
 const auth2 = (req ,res, next)=>{
@@ -48,10 +48,13 @@ const auth2 = (req ,res, next)=>{
   }
 }
 
+
+// application level middleware
 // server.use(auth);
 
 
 // API or endpoints 
+// router level middleware usage example
 // server.get('/', auth1 ,(req, res) =>{
 //    res.json({type : 'GET'});
 // })
