7d3406fc08812b534f70b6b722ea9f94f0eecc2e
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-01-06 09:27:59 +0530

    ch-3 assignment

---
+++ ch-3-expressjs/assignment.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const server = express();
+
+// // assignment 1
+// server.get('/demo', (req , res)=>{
+//     const name = req.query.name;
+//     const age = req.query.age;
+//     const subject = req.query.subject;
+//     console.log(req.query);
+//     res.send(req.query);
+// })
+
+// assignment 2
+server.get('/demo/:name/:age/:subject', (req , res)=>{
+    const msg = req.params;
+    console.log(msg);
+    res.json(msg);
+})
+
+
+
+
+
+server.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+})
+
