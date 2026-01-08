a19461ff6b9ec4a6edccb98e8c04c1fa660f8c1b
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-01-08 10:51:20 +0530

    ch-4 assignment

---
+++ ch-4-Rest-API/assignment.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+
+const data = JSON.parse(fs.readFileSync('qouts.json', 'utf-8'));
+const qouts = data.quotes;
+
+
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+//GET method 
+app.get('/qouts', (req, res) => {
+    res.send(qouts);
+})
+app.get('/qouts/:id', (req, res) => {
+    const id = +req.params.id;
+    const singleqout = qouts.find(p => p.id === id);
+    res.send(singleqout);
+})
+
+// create 
+app.post('/qouts', (req, res) => {
+    qouts.push(req.body);
+    console.log(req.body);
+    res.json(req.body);
+
+})
+
+// update
+app.put('/qouts/:id', (req, res) => {
+    const id = +req.params.id;
+    const singleqout = qouts.findIndex(p => p.id === id);
+    qouts.splice(singleqout, 1, req.body);
+    res.json(req.body);
+})
+
+// patch
+app.patch('/qouts/:id', (req, res) => {
+    const id = +req.params.id;
+    const singleqout = qouts.findIndex(p => p.id === id);
+    const qout = qouts[singleqout];
+    qouts.splice(singleqout, 1, { ...qout, ...req.body });
+    res.json(req.body);
+})
+
+// delete
+app.delete('/qouts/:id', (req, res) => {
+    const id = +req.params.id;
+    const singleqout = qouts.findIndex(p => p.id === id);
+    const qout = qouts[singleqout];
+    qouts.splice(singleqout);
+    res.status(201).json(qout);
+})
+
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000");
+})
