5d43889c6b98cff65435bb979c3c51720b49fef9
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-01-18 16:11:51 +0530

    server side rendering

---
+++ ch-10-server-side-rendering/server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const productRoutes = require('./routes/product');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/ecommerce')
+  .then(() => console.log('DB connected'));
+
+app.use('/products', productRoutes);
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
---
+++ ch-10-server-side-rendering/server/routes/product.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const productController = require('../controller/product');
+
+const router = express.Router();
+
+// // through router , now these routers are independent to each other
+router
+    .post('/', productController.createproduct)
+    .get('/', productController.getAllproducts)
+    // .get('/:id', productController.getsingleprod)
+    // .put('/:id', productController.updateprod)
+    // .patch('/:id', productController.updateprod2)
+    .delete('/:id', productController.deleteprod)
+
+router.get('/ssr', productController.getAllProductsSSR);    
+
+
+// exports.router = router;  
+module.exports = router;  
