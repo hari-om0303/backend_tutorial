352f763098073dff9ca2f4b19005bbb9befd1368
Hari Om Gupta <hariomgupta0303@gmail.com>
2026-01-29 19:59:11 +0530

    class error handling

---
+++ ch-13-error_handling/app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const ExpressError = require('./ExpressError');
+const app = express();
+
+const checkToken = (req , res ,next)=>{
+    let {token} = req.query;
+    if(token === 'giveaccess'){
+       next();
+    }
+    throw new ExpressError(401 , "Access Denied");
+}
+
+app.get('/', (req , res)=>{
+    res.send('Hello World');
+})
+
+app.get('/err', (req ,res)=>{
+    abcd = abcd;
+})
+
+app.get('/api' , checkToken , (req , res)=>{
+    res.send('Welcome to the API');
+});
+
+// // error as object
+// app.use((err ,req,res ,next)=>{
+//     let {status = 404 , message = 'Not Found'} = err || {};
+//     res.status(status).json({message});
+// })
+
+// as custom error class
+app.use((err ,req,res ,next)=>{
+    let {status , message} = err;
+    res.status(status).send({message});
+})
+    
+app.listen(3000 , ()=>{
+    console.log('Server is running on port 3000');
+})
